a062a1b24f74e862afc06593dfcd39f2e49f9dda
Mohammad Al Amin <alaminbamna08@gmail.com>
2025-01-30 11:58:24 +0600

    backend project setup

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,44 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    // TypeScript-specific settings
+    languageOptions: {
+      parser: tseslint.parser,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  {
+    // Prettier Integration
+    plugins: { prettier: prettierPlugin },
+    rules: {
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    ignores: ['node_modules', 'dist', 'build'],
+  },
+]
